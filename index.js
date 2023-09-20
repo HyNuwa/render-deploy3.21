45998acc3fb92c54688cbe867151b2bb2e23ce70
NuwaMax <hweilnuwa@gmail.com>
2023-09-20 01:24:50 -0300

    render-backend3

--- index.js
+++ index.js
@@ -16,52 +16,7 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 })
-/*app.get('/info', (request, response) => {
-  const todayDate = new Date(Date.now());
-  const maxPerson = persons.length
-  response.send(`the phonebook has info for ${maxPerson} people ${todayDate}`);
-})
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const selectedPerson = persons.find(person => person.id === id)
-  response.json(selectedPerson)
-})
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)  
-  response.status(204).end()
-})
-const generateId=()=>{
-  const maxId = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0
-  const randomId = Math.floor(Math.random(maxId+1)*100)
-  return randomId;
-}*/
-/*app.post('/api/persons', (request, response) => {
-  const body = request.body;
 
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  }else if(!body.number){
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
-  }
-  if(persons.find(person=>person.name===body.name)){
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
-  const person = {
-    id: generateId(),
-    name: body.name || false,
-    number: body.number,
-  }
-  persons = persons.concat(person);
-  console.log(person);
-  response.json(person);
-})*/
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
